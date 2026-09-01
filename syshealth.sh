f60cc7564babe950aa67475e0c578a87a3d6a6c0
Student <student@localhost.localdomain>
2026-09-01 15:12:30 -0400

    ADD shebang and make syshealth.sh executable

---
+++ syshealth.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# ==============================
+# syshealth.sh - System health $ Log analysis toolkit
+# Lab 1 - Data Collector
+# Author - Saran saai dommaraju
+# Date - $(date +%Y-%m-%d)
+# ==============================
+
+

853ddc0f983ef06d4a967b8790fb15e68fd01f55
saran7207 <saransaai07@gmail.com>
2026-09-02 03:26:32 -0400

    add positional handling and report output

--- syshealth.sh
+++ syshealth.sh
@@ -17,10 +17,12 @@ echo "Hostname without quotes: $HOSTNAME" # works but causes word splitting bug
 echo "Hostname with quotes: \"$HOSTNAME\"" #safe
 
 cat << EOF
+
 # Comment for grader
 # In python or java variables expand safely
 # bash unquoted variables splits on spaces/tabs/newlines
 # we always use double quote unless we want to split the variables
+
 EOF
 
 # --- system metrics collection ---
@@ -28,3 +30,43 @@ UPTIME=$(uptime -p)
 DISK_USAGE=$(df -h / | tail -1)
 MEMORY_USAGE=$(free -h | awk '/Mem:/ {print $3 "/" $2}')
 PROCESS_COUNT=$(ps -e | wc -l)
+
+# --- output handling ---
+OUTPUT_FILE="${1:-}" # if argument is given use it, or else go default
+
+print_report() 
+{
+printf "=======\n"
+printf "HOSTNAME: %s\n" "$HOSTNAME"
+printf "UPTIME: %s\n" "$UPTIME"
+printf "DISK USAGE: %s\n" "$DISK_USAGE"
+printf "MEMORAGE USAGE: %s\n" "$MEMORY_USAGE"
+printf "PROCESS COUNT: %s\n" "$PROCESS_COUNT"
+printf "=======\n"
+}
+
+if [ -n "$OUTPUT_FILE" ]; then
+	print_report > "$OUTPUT_FILE"
+	echo "report written to $OUTPUT_FILE"
+else
+	print_report
+fi
+
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

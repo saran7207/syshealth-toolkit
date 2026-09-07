123591a60423f4f916b10d7b707bd217f7b1bc40
saran7207 <saransaai07@gmail.com>
2026-09-07 03:42:34 -0400

    Updated

--- syshealth.sh
+++ syshealth.sh
@@ -6,45 +6,45 @@
 # Date - $(date +%Y-%m-%d)
 # ==============================
 
-# -- variables and coating demonstration --
+# Variables and demonstrating how to quote
 HOSTNAME=$(hostname)
 CURRENT_DATE=$(date '+%Y-%m-%d %H:%M:%S')
 
-# important quoting demo
-# without quotes there will be word splitting bug
-# with double quotes its safe
+# When we use without quotes, word splitting bug occurs.
+# So we use double quotes which is safer
 echo "Hostname without quotes: $HOSTNAME" # works but causes word splitting bug
 echo "Hostname with quotes: \"$HOSTNAME\"" #safe
 
 cat << EOF
 
-# Comment for grader
-# In python or java variables expand safely
-# bash unquoted variables splits on spaces/tabs/newlines
-# we always use double quote unless we want to split the variables
+# Word splitting occurs in bash whenever we use unquoted variables. It splits the variables on spaces, tabs or newlines
+# So we should always use double quoting unless we want splitting to happen
 
 EOF
 
-# --- system metrics collection ---
-UPTIME=$(uptime -p)
-DISK_USAGE=$(df -h / | tail -1)
-MEMORY_USAGE=$(free -h | awk '/Mem:/ {print $3 "/" $2}')
-PROCESS_COUNT=$(ps -e | wc -l)
+# Collecting system metrics
+UPTIME=$(uptime -p) # how long the machine is running
+DISK_USAGE=$(df -h / | tail -1) # storage usage
+MEMORY_USAGE=$(free -h | awk '/Mem:/ {print $3 "/" $2}') # RAM usage
+PROCESS_COUNT=$(ps -e | wc -l) # processes running
 
-# --- output handling ---
+# Handling the output
 OUTPUT_FILE="${1:-}" # if argument is given use it, or else go default
 
+# Function to print system report
 print_report() 
 {
-printf "=======\n"
-printf "HOSTNAME: %s\n" "$HOSTNAME"
-printf "UPTIME: %s\n" "$UPTIME"
-printf "DISK USAGE: %s\n" "$DISK_USAGE"
-printf "MEMORAGE USAGE: %s\n" "$MEMORY_USAGE"
-printf "PROCESS COUNT: %s\n" "$PROCESS_COUNT"
-printf "=======\n"
+printf "====================\n"
+printf "System Health Report - %s\n" "$CURRENT_DATE"
+printf "Hostname     : %s\n" "$HOSTNAME"
+printf "Uptime       : %s\n" "$UPTIME"
+printf "Disk /       : %s\n" "$DISK_USAGE"
+printf "Memory used  : %s\n" "$MEMORY_USAGE"
+printf "Total processes : %s\n" "$PROCESS_COUNT"
+printf "====================\n"
 }
 
+# To check if there was an argument given 
 if [ -n "$OUTPUT_FILE" ]; then
 	print_report > "$OUTPUT_FILE"
 	echo "report written to $OUTPUT_FILE"
@@ -52,6 +52,7 @@ else
 	print_report
 fi
 
+# Exit status code
 exit 0
 
 
